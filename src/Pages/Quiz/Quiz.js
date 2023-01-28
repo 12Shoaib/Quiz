8325c0444602f0f73d4e65253b2613218cf5402a
shoaib <shoaibmohd916@gmail.com>
2023-01-28 15:03:14 +0530

    The finalised changes are updated

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -3,6 +3,7 @@ import Button from '../../Atom/Button/Button'
 import {Topic1 , Topic2 , Topic3 , inputValue ,isGeneralValue ,isTechnicalValue,isMathsValue } from '../../Recoil/RecoilAtom'
 import {useRecoilValue , useRecoilState} from 'recoil'
 import { useState} from 'react'
+import {GiPartyPopper} from 'react-icons/gi'
 
 const Quiz = () => {
     const General = useRecoilValue(Topic1)
@@ -13,13 +14,13 @@ const Quiz = () => {
     const [isTechnical , setIsTechnical] = useRecoilState(isTechnicalValue)
     const [isMaths , setIsMaths] = useRecoilState(isMathsValue)
     const [count , setCount] = useState(0)
-    const [isSubmit , setIsSubmit] = useState(false)
     const [score , setScore ] = useState(0)
+    const [resultCard , setResultCard] = useState(false)
 
     function Rendering(){      
         if(inputCapture === 'General Quiz'){
           setIsGeneral(true)
-          General[count].options,map((element)=> element.true)  
+          General[count].options.map((element)=> element.true)  
           setIsTechnical(false)
           setIsMaths(false)  
         } 
@@ -39,31 +40,44 @@ const Quiz = () => {
     function handleNextClick() {
         if( count< General.length){
         setCount(count + 1)
-        }else{
-         setIsSubmit(true) 
-        } 
-         
+        }         
+    }
+    function handleInputCapture(e){
+      const userInput = e.target.value
+      if(userInput === 'true'){
+        setScore(score + 2)
+      }
+    }
+    function handleSubmitButton(){
+      setResultCard(true)
     }
 
     return (
         <div className={quiz.mainComponent}>
+           {resultCard ? 
+            <div className={quiz.content}>
+                <h1><GiPartyPopper className={quiz.partyIcon} /> Congratulations Your score is {score} <GiPartyPopper className={quiz.partyIcon} /></h1>
+            </div> 
+
+           :
+
             <div className={quiz.content}>
             <h2 className={quiz.heading}>Question  <span className={quiz.question}>{count}</span><span className={quiz.number}>/5</span></h2> 
               {isGeneral && <><h4 className={quiz.heading2}>{General[count].question}</h4>
-              {General[count].options.map((element) => <p className={quiz.options}> <input  type='radio' name='radio' /> {element.option} </p> )}</>}
+              {General[count].options.map((element) => <p className={quiz.options}> <input onChange={handleInputCapture} value={element.isCorrect}  type='radio' name='radio' /> {element.option} </p> )}</>}
 
 
-              {isTechnical &&  <><h4 className={quiz.heading2}>{Technical[0].question}</h4>
-              {Technical[0].options.map((element)=> <p  className={quiz.options}> <input type='radio' name='radio' />{element.option}</p>)}</> }
+              {isTechnical &&  <><h4 className={quiz.heading2}>{Technical[count].question}</h4>
+              {Technical[count].options.map((element)=> <p  className={quiz.options}> <input onChange={handleInputCapture} value={element.isCorrect}  type='radio' name='radio' />{element.option}</p>)}</> }
               
 
-              {isMaths &&  <><h4 className={quiz.heading2}>{Mathematics[0].question}</h4>
-              {Mathematics[0].options.map((element)=> <p className={quiz.options}> <input  type='radio' name='radio' />{element.option}</p>)}</>} 
+              {isMaths &&  <><h4 className={quiz.heading2}>{Mathematics[count].question}</h4>
+              {Mathematics[count].options.map((element)=> <p className={quiz.options}> <input onChange={handleInputCapture} value={element.isCorrect} type='radio' name='radio' />{element.option}</p>)}</>} 
 
               <div className={quiz.button}>
-               {isSubmit ? <Button  Name='Submit'/> : <Button onClick={handleNextClick} Name='Next'  />}
+               {count===4 ? <Button onClick={handleSubmitButton}  Name='Submit'/> : <Button onClick={handleNextClick} Name='Next'  />}
             </div>  
-            </div>
+            </div> }
             
         </div>
     )
